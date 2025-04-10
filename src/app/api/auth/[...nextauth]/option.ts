26a5a91707ca49cc8a22f8d467989135cb285ad4
lalitcap23 <lalitrajput232002@gmail.com>
2025-04-10 20:50:09 +0530

    fs

--- src/app/api/auth/[...nextauth]/option.ts
+++ src/app/api/auth/[...nextauth]/option.ts
@@ -1,6 +1,6 @@
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcrypt"; 
+import bcrypt from "bcrypt";
 import { connectDB } from "../../../lib/helpling/dbconnection";
 import { UserModal } from "../../../../models/user";
 
@@ -10,46 +10,65 @@ export const authOptions: NextAuthOptions = {
       name: "Credentials",
       credentials: {
         username: {
-          label: "username",
-          placeholder: "username",
+          label: "Username",
+          placeholder: "Enter your username",
           type: "text",
         },
         password: {
-          label: "password",
-          placeholder: "password",
+          label: "Password",
+          placeholder: "Enter your password",
           type: "password",
-        }
+        },
       },
       async authorize(credentials: any): Promise<any> {
         await connectDB();
         try {
-          const user = await UserModal.findOne({ 
+          const user = await UserModal.findOne({
             $or: [
               { username: credentials.username },
-              { email: credentials.username }
-            ]
+              { email: credentials.username },
+            ],
           });
-          
+
           if (!user) {
             throw new Error("User not found");
           }
-          
+
           if (!user.verified) {
             throw new Error("User not verified");
           }
-          
-          const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password); 
-          
+
+          const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password);
+
           if (isPasswordCorrect) {
             return user;
           } else {
             throw new Error("Invalid password");
           }
         } catch (err: any) {
-          console.log("Error connecting to database:", err);
+          console.error("Error during authentication:", err.message);
           return null;
         }
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/Sign-in',
+  },
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.user = user;
       }
-    })
-  ]
+      return token;
+    },
+    async session({ session}) {
+      
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
 };
